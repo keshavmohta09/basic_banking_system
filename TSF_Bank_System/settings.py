d3437262624160f66d018456e4e9a0c5f12a0cb1
keshavmohta09 <68001229+keshavmohta09@users.noreply.github.com>
2021-04-18 00:49:21 +0530

    commit changes

--- TSF_Bank_System/settings.py
+++ TSF_Bank_System/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1','basic-banking-system-09.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','basic-banking-system-09.herokuapp.com/']
 
 
 # Application definition
